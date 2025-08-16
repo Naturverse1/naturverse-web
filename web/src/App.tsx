4c8194a5101e4c7935dfe0cf5db7797ab433b146
Naturverse1 <turianmediacompany@gmail.com>
2025-08-16 08:11:14 -0700

    Add header navigation and placeholder pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
 import Landing from "./pages/Landing";
+import Map from "./pages/Map";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
---
+++ web/src/pages/Map.tsx
@@ -0,0 +1,1 @@
+export default function Map(){ return <main className="landing"><h1>Map</h1><p>Worlds will go here.</p></main>; }
