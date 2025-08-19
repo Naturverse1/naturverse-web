8be73cfa80319e0b1b19961b6f189de147b58cec
Naturverse1 <turianmediacompany@gmail.com>
2025-08-19 08:37:47 +0700

    fix: configure netlify for vite app

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
-import ErrorBoundary from './ErrorBoundary';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+function Root() {
+  return (
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  );
+}
+
+const el = document.getElementById("root")!;
+ReactDOM.createRoot(el).render(<Root />);
