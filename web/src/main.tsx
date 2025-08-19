3080d1fec9037facf917e0b131cc24c717afb731
Naturverse1 <turianmediacompany@gmail.com>
2025-08-19 23:29:31 +0700

    feat: configure Netlify SPA routing and React app

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const rootEl = document.getElementById("root")!;
-createRoot(rootEl).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
