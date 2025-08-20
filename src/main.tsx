3b3b019dc038a689eda35c4c519a87fe95e6dbcb
Naturverse1 <turianmediacompany@gmail.com>
2025-08-20 21:10:27 +0700

    chore(security): set CSP compatible with Vite and temporarily clear client caches

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,17 @@
+// Hard-kill any previously registered service workers (from older builds)
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((regs) => {
+      for (const r of regs) r.unregister();
+    })
+    .catch(() => {});
+  // Also stop the active controller so reload pulls fresh assets
+  if (navigator.serviceWorker.controller) {
+    navigator.serviceWorker.controller.postMessage({ type: "SKIP_WAITING" });
+  }
+}
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
