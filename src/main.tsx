4c6565e572c150b0e56c61891c06d9fd381549ef
Naturverse1 <turianmediacompany@gmail.com>
2025-08-20 22:30:47 +0700

    Restore router and layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
-const root = document.getElementById("root")!;
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const rootEl = document.getElementById('root')!;
+createRoot(rootEl).render(<RouterProvider router={router} />);
