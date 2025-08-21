4485621782073de7ec15a855bf6e5381bbfd0718
Naturverse1 <turianmediacompany@gmail.com>
2025-08-21 23:42:03 +0700

    Restore batch 286 layout, hero, nav, and culture

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,5 +1,3 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="nv-page nv-container">{children}</main>
-  );
+export default function Layout({children}:{children:React.ReactNode}){
+  return <main className="nv-page nv-container">{children}</main>;
 }
