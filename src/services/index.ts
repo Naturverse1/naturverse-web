1b5015f283666c1b46e244818092dfbf8681a327
Naturverse1 <turianmediacompany@gmail.com>
2025-08-23 14:27:50 +0700

    feat: add quizzes and feedback helpers

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,3 +6,5 @@ export * from '../lib/storage';
 export * from '../lib/auth';
 export * from '../lib/queries';
 export * from '../lib/xp';
+export * from '../lib/quizzes';
+export * from '../lib/feedback';
