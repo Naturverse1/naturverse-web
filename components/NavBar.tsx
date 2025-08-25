88bbce7c5a8df2a2bffb7d83f320e14fdb579271
Naturverse1 <turianmediacompany@gmail.com>
2025-08-25 13:33:54 +0700

    fix: auth-aware nav and home tiles

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,6 +5,7 @@ import { useAuth } from '@/lib/auth-context';
 
 export default function NavBar() {
   const { ready, user } = useAuth();
+  const authed = Boolean(user);
   const headEmoji =
     (user?.user_metadata?.navatarEmoji as string) ??
     (user?.user_metadata?.avatar_emoji as string) ??
@@ -16,15 +17,26 @@ export default function NavBar() {
         <Link className="nv-brand" href="/">Naturverse</Link>
 
         <nav className="nv-links">
-          <Link href="/worlds">Worlds</Link>
-          <Link href="/zones">Zones</Link>
-          <Link href="/marketplace">Marketplace</Link>
-          <Link href="/wishlist">Wishlist</Link>
-          <Link href="/naturversity">Naturversity</Link>
-          <Link href="/naturbank">NaturBank</Link>
-          <Link href="/navatar">Navatar</Link>
-          <Link href="/passport">Passport</Link>
-          <Link href="/turian">Turian</Link>
+          <Link className="nv-link" href="/worlds">Worlds</Link>
+          <Link className="nv-link" href="/zones">Zones</Link>
+          <Link className="nv-link" href="/marketplace">Marketplace</Link>
+          <Link className="nv-link" href="/wishlist">Wishlist</Link>
+          <Link className="nv-link" href="/naturversity">Naturversity</Link>
+          <Link className="nv-link" href="/naturbank">NaturBank</Link>
+          {authed ? (
+            <Link className="nv-link" href="/navatar">Navatar</Link>
+          ) : (
+            <a
+              href="#"
+              aria-disabled
+              className="nv-link is-disabled"
+              onClick={(e) => e.preventDefault()}
+            >
+              Navatar
+            </a>
+          )}
+          <Link className="nv-link" href="/passport">Passport</Link>
+          <Link className="nv-link" href="/turian">Turian</Link>
           {ready && user ? (
             <>
               <Link className="nv-cart" href="/cart" aria-label="Cart">
