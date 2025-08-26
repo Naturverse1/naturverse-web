291e78a17c47219acd0565dd8e902b18fc7a81a5
Naturverse1 <turianmediacompany@gmail.com>
2025-08-26 14:26:12 +0700

    feat: add route effects and magic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { organizationLd, websiteLd } from './lib/jsonld';
 import { CartProvider } from './lib/cart';
 import ToasterListener from './components/Toaster';
+import RouteFX from './components/RouteFX';
+import './styles/magic.css';
 
 export default function App() {
   return (
     <CartProvider>
       <>
+        {/* Global route visual effects (page fade-in) */}
+        <RouteFX />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationLd) }}
@@ -16,13 +21,14 @@ export default function App() {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteLd) }}
         />
-          <main id="main" className="nv-page">
-            <div className="container">
-              <RouterProvider router={router} />
-            </div>
-          </main>
+        <main id="main" className="nv-page">
+          <div className="container">
+            <RouterProvider router={router} />
+          </div>
+        </main>
         <ToasterListener />
       </>
     </CartProvider>
   );
 }
+
