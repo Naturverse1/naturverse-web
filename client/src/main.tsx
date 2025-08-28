379dfd0ced89d94728d3b1fc0ea75966f5c418f5
Naturverse1 <turianmediacompany@gmail.com>
2025-08-28 10:11:20 +0700

    add boot error fallback for client app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+try {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+  console.log('[boot] rendered');
+} catch (err) {
+  console.error('[boot] render failed', err); // show full stack
+  document.body.innerHTML = "<pre style='color:red'>" + err + '</pre>';
+}
