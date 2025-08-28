92f35fc6c23c79d8fed0d860a8e1d56e5033a4ea
Naturverse1 <turianmediacompany@gmail.com>
2025-08-28 13:04:55 +0700

    chore(pwa): disable service worker & force-unregister

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+// TEMP: ensure no service worker remains. This prevents the “Offline” page.
+if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations?.()
+    .then((rs) => rs.forEach((r) => r.unregister()))
+    .catch(() => {});
+}
+
 try {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
