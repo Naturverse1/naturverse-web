fd09114eeda8a47ea9257fd96fd703433a701c56
Naturverse1 <turianmediacompany@gmail.com>
2025-08-29 13:06:47 +0700

    Add sitemap and robots Netlify functions

---
+++ netlify/functions/sitemap.ts
@@ -0,0 +1,22 @@
+// Dynamic sitemap function
+// Path: /.netlify/functions/sitemap  (routed from /sitemap.xml via netlify.toml)
+export default async () => {
+  const base = 'https://thenaturverse.com';
+  // TODO: expand this list or generate from content
+  const urls = [
+    '/', '/worlds', '/zones', '/marketplace',
+    '/wishlist', '/naturversity', '/naturbank', '/navatar', '/passport'
+  ];
+
+  const body =
+    `<?xml version="1.0" encoding="UTF-8"?>\n` +
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
+    urls.map(u =>
+      `  <url><loc>${base}${u}</loc><changefreq>daily</changefreq><priority>0.7</priority></url>`
+    ).join('\n') +
+    `\n</urlset>\n`;
+
+  return new Response(body, {
+    headers: { 'Content-Type': 'application/xml; charset=UTF-8' }
+  });
+};
