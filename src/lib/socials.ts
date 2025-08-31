84c4073c16643078011802ea3865b2e7f6acd84f
Naturverse1 <turianmediacompany@gmail.com>
2025-08-31 21:56:57 +0700

    chore: add socials helper

---
+++ src/lib/socials.ts
@@ -0,0 +1,7 @@
+export const SOCIALS = [
+  { name: 'X', href: 'https://x.com/TuriantheDurian' },
+  { name: 'Instagram', href: 'https://instagram.com/turianthedurian' },
+  { name: 'TikTok', href: 'https://www.tiktok.com/@turian.the.durian' },
+  { name: 'YouTube', href: 'https://youtube.com/@TuriantheDurian' },
+  { name: 'Facebook', href: 'https://facebook.com/TurianMediaCompany' },
+];
