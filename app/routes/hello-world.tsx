3883877be544583cb9ed7b31da15b54cbd6c772e
Naturverse1 <turianmediacompany@gmail.com>
2025-08-31 14:53:26 +0700

    feat: add hello world route

---
+++ app/routes/hello-world.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HelloWorld() {
+  return (
+    <main className="container">
+      <h1>🌱 Hello Naturverse!</h1>
+      <p>This is a test deploy preview page.</p>
+      <a href="/">← Back to Home</a>
+    </main>
+  );
+}
+
