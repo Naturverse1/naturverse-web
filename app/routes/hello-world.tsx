5f61fb802257590e15d0757af637944ec9f5029e
Naturverse1 <turianmediacompany@gmail.com>
2025-08-31 15:13:40 +0700

    Add hello world route and remove Stripe React dependency

---
+++ app/routes/hello-world.tsx
@@ -0,0 +1,9 @@
+export default function HelloWorld() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>🌱 Hello Naturverse!</h1>
+      <p>This is a test deploy preview page.</p>
+      <a href="/">← Back to Home</a>
+    </div>
+  );
+}
