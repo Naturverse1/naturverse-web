3e7068c51b5a0748f0c6a9a1b02a83394f128bb9
Naturverse1 <turianmediacompany@gmail.com>
2025-08-30 22:34:41 -0700

    baseline: clean routing/headers + CSP-safe OAuth callback (from good deploy)

---
+++ public/auth/callback.js
@@ -0,0 +1,12 @@
+﻿(() => {
+  try {
+    if (location.hash && location.hash.startsWith("#")) {
+      sessionStorage.setItem("nv_oauth_hash", location.hash);
+    }
+    if (location.search && location.search.startsWith("?")) {
+      sessionStorage.setItem("nv_oauth_search", location.search);
+    }
+  } finally {
+    location.replace("/");
+  }
+})();
