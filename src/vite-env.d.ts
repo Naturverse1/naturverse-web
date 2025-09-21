ab98c6f81ceec2cc2576fffd0f7dc8598a4c6080
Naturverse1 <turianmediacompany@gmail.com>
2025-09-21 21:03:01 +0700

    Switch navatar generator to Hugging Face FLUX

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,9 @@ interface ImportMetaEnv {
   readonly VITE_SITE_URL?: string;
   readonly VITE_ENABLE_AI?: string;
   readonly VITE_ENABLE_AUTO_STAMPS?: string;
+  readonly HUGGINGFACE_API_KEY: string;
+  readonly NAVATAR_BRAND_STYLE?: string;
+  readonly NAVATAR_NEGATIVE?: string;
 }
 
 interface ImportMeta {
